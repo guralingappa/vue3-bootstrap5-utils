cd4c88f6339c8119a63f21f87296b6895bc2cc75
Guralingappa <guralingappak@hotmail.com>
2022-07-23 14:24:10 +0530

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,59 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <NavBar></NavBar>
+  <div class="container-fluid">
+    <div class="row">
+      <section class="col-md-12">
+        <header class="col-md-3">Single select dropdown</header>
+          <form class="row">
+         <div class="col-md-6">
+        <SingleSelect
+          :options="options"
+          v-model="user"
+          :showLabel="true"
+          :label="'User'"
+        ></SingleSelect>
+      </div>
+    </form>
+      </section>
+      <section class="col-md-12">
+        <header class="col-md-3">Single select dropdown with placeholder</header>
+          <form class="row">
+         <div class="col-md-6">
+        <SingleSelect
+          :options="options"
+          v-model="user"
+          :showDefaultText="true"
+          :defaultText="'Select user......'"
+          :showLabel="true"
+          :label="'User'"
+        ></SingleSelect>
+      </div>
+    </form>
+      </section>
+    </div>  
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import NavBar from "./components/NavBar.vue";
+import SingleSelect from "./components/SingleSelect.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    NavBar,
+    SingleSelect,
+  },
+  data() {
+    return {
+      options: [
+        { text: "Guru", value: "1" },
+        { text: "Chandru", value: "2" },
+      ],
+      user: "1",
+    };
+  },
+};
 </script>
 
 <style>
@@ -21,6 +63,5 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/components/SingleSelect.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container g-single-select">
+    <div class="row">
+      <label class="form-label col" for="singleselect" v-if="showLabel">{{
+        label
+      }}</label>
+      <select
+        id="singleselect"
+        v-model="model"
+        class="form-select col"
+      >
+        <option v-if="showDefaultText">{{ defaultText }}</option>
+        <option
+          v-for="option in options"
+          :key="option.value"
+          :value="option.value"
+        >
+          {{ option.text }} <img alt="text" width="10px" height="10px" />
+        </option>
+      </select>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleSelect",
+  props: {
+    modelValue: [String, Number],
+    options: {
+      type: Array,
+      default: function () {
+        return [];
+      },
+    },
+    showLabel: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    },
+    showDefaultText: {
+      type: Boolean,
+      default: false,
+    },
+    defaultText: {
+      type: String,
+    },
+  },
+  computed: {
+    model: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit("update:modelValue", value);
+      },
+    },
+  },
+};
+</script>
+
+<style type="css">
+.g-single-select label.form-label {   
+        margin:auto 0;
+        text-align: right;  
+}
+</style>
